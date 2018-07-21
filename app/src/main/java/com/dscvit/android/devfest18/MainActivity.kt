3247e8361a49c91573540093b69a725c507c53f3
Yaswant Narayan <yoyaswant@gmail.com>
2018-07-21 14:20:52 +0530

    Initial commit

---
+++ app/src/main/java/com/dscvit/android/devfest18/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.dscvit.android.devfest18
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dscvit.android.devfest18.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+
+}
